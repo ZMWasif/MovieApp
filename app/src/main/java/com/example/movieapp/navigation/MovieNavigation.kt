a39a74e4e21db9f62c3788bd9ffb4e19e7d887f3
ZMWasif <ziauddinwasif@gmail.com>
2023-06-25 18:42:23 +0200

    Navigation Started

--- app/src/main/java/com/example/movieapp/navigation/MovieNavigation.kt
+++ app/src/main/java/com/example/movieapp/navigation/MovieNavigation.kt
@@ -1,9 +1,11 @@
 package com.example.movieapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.movieapp.screens.home.HomeScreen
 import com.example.movieapp.screens.details.DetailsScreen
 
@@ -18,7 +20,11 @@ fun MovieNavigation() {
             HomeScreen(navController = navController)
         }
 
-        composable(MovieScreens.DetailsScreen.name+ "/{movie}"){
+        composable(MovieScreens.DetailsScreen.name+ "/{movie}",
+        arguments = listOf(navArgument(name = "movie"){
+            type = NavType.StringArrayType
+        })
+            ){
             DetailsScreen(navController= navController)
         }
 
